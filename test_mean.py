3b7719a7fd9387d3ddcf603915879f1e8abc485b
Johan Fruh <bsjfru@leeds.ac.uk>
2017-01-05 15:57:38 +0000

    r Please enter the commit message for your changes. Lines starti

--- test_mean.py
+++ test_mean.py
@@ -3,6 +3,6 @@ from mean import *
 def test_ints():
 	input = [1, 2, 3, 4, 5]
 	calculated_value = mean(input)
-	expected_value = 3
+	expected_value = 4
 	assert calculated_value == expected_value
 
